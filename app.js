7d088e2405e529a9b5f4c7cb8ea0b6c58691bc3a
huntdavid175 <huntdavid175@gmail.com>
2020-06-05 09:18:17 +0100

    added various functionalities

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 const numbers = document.querySelectorAll(".number");
-const operator = document.querySelectorAll(".operator");
+const operators = document.querySelectorAll(".operator");
 const display = document.querySelector(".display");
+const equal = document.querySelector("#equal")
+console.log(equal)
+let temp;
+let op;
 
 numbers.forEach(function(number){
     number.addEventListener("click",function(e){
-        display.textContent += e.target.textContent
+        displayToScreen(e)
+        
     })
 });
 
+operators.forEach(function(operator){
+    operator.addEventListener("click",function(e){
+        calc(e)
+    })
+})
+
+equal.addEventListener("click",function(e){
+    operate(temp,op)
+    // console.log(op)
+})
 
 
+// Display to screen function 
+function displayToScreen(e){
+    if(display.textContent == "0"){
+        display.textContent = ""
+        display.textContent += e.target.textContent
+    }
+    else{
+        display.textContent += e.target.textContent
+    }
+}
 
-// functions for operations  
-
+// functions for operations                
 function add(a,b){
     return (a + b);
 }
@@ -27,4 +51,28 @@ function multiply(a,b){
 
 function divide(a,b){
     return (a / b);
+}
+
+
+
+function calc(e){
+   temp = display.textContent;
+   op = e.target.textContent;
+   display.textContent = ""
+}
+
+function operate(temp,op){
+    temp = parseInt(temp);
+    if (op == "x"){
+        display.textContent = multiply(temp,parseInt(display.textContent))
+    }
+    if(op == "+"){
+        display.textContent = add(temp,parseInt(display.textContent))
+    }
+    if(op == "-"){
+        display.textContent = subtract(temp,parseInt(display.textContent))
+    }
+    if(op == "/"){
+        display.textContent = divide(temp,parseInt(display.textContent))
+    }
 }
