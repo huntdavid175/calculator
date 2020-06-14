5f27f984a8dcaa3a9f664002c879a4a3b7d7990e
huntdavid175 <huntdavid175@gmail.com>
2020-06-14 10:34:43 +0100

    formatted code

--- app.js
+++ app.js
@@ -4,92 +4,82 @@ const display = document.querySelector(".display");
 const equal = document.querySelector("#equal");
 const clear = document.querySelector(".clear");
 const boxes = document.querySelectorAll(".box");
-let state = 0
+let state = 0;
 let temp;
 let op;
 let sol;
 
-numbers.forEach(function(number){
-  number.addEventListener("click",function(e){
-    removeSelected();
-    e.target.classList.add("selected")
-    showDisplay(e)
-  })
-})
+numbers.forEach(function (number) {
+	number.addEventListener("click", function (e) {
+		removeSelected();
+		e.target.classList.add("selected");
+		showDisplay(e);
+	});
+});
 
-operators.forEach(function(operator){
-  operator.addEventListener("click",function(e){
-    removeSelected()
-    e.target.classList.add("selected")
-    chooseOperator(e)
-    console.log(op)
-  })
-})
+operators.forEach(function (operator) {
+	operator.addEventListener("click", function (e) {
+		removeSelected();
+		e.target.classList.add("selected");
+		chooseOperator(e);
+		console.log(op);
+	});
+});
 
-clear.addEventListener("click",clearCalc)
-equal.addEventListener("click",function(){
-  if(sol){
-    operate(sol,op);
-    display.textContent = sol
-    op = undefined
-  }
-  else{
-    operate(temp,op)
-    display.textContent = sol
-    op = undefined
-  }
-})
+clear.addEventListener("click", clearCalc);
+equal.addEventListener("click", function () {
+	if (sol) {
+		operate(sol, op);
+		display.textContent = sol;
+		op = undefined;
+	} else {
+		operate(temp, op);
+		display.textContent = sol;
+		op = undefined;
+	}
+});
 
+// show display function
 
-// show display function 
-
-function showDisplay(e){
-  if(e.target.textContent == "."){
-    if(!display.textContent.includes(".")){
-      display.textContent += e.target.textContent
-    }
-  }
-  if (e.target.textContent != "."){
-     if (state == 0){
-    state++
-    display.textContent = ""
-    display.textContent = e.target.textContent
-  }
-  else{
-     display.textContent += e.target.textContent;
-    state++
-  }
-  }
- 
+function showDisplay(e) {
+	if (e.target.textContent == ".") {
+		if (!display.textContent.includes(".")) {
+			display.textContent += e.target.textContent;
+		}
+	}
+	if (e.target.textContent != ".") {
+		if (state == 0) {
+			state++;
+			display.textContent = "";
+			display.textContent = e.target.textContent;
+		} else {
+			display.textContent += e.target.textContent;
+			state++;
+		}
+	}
 }
 
+// choose operator function
 
-// choose operator function 
-
-function chooseOperator(e){
-  if (sol){
-    operate(sol,op)
-    display.textContent = sol
-    op = e.target.textContent
-    state = 0
-  }
-  else{
-     if(temp){
-    operate(temp,op)
-    op =e.target.textContent
-    display.textContent = sol
-    state = 0
-    
-  }
-  else{
-      op = e.target.textContent
-      temp = display.textContent
-      state = 0
-  // display.textContent = ""
-  }
-  }
- 
-
+function chooseOperator(e) {
+	if (sol) {
+		operate(sol, op);
+		display.textContent = sol;
+		op = e.target.textContent;
+		state = 0;
+	} else {
+		if (temp) {
+			operate(temp, op);
+			op = e.target.textContent;
+			display.textContent = sol;
+			state = 0;
+		} else {
+			op = e.target.textContent;
+			temp = display.textContent;
+			state = 0;
+			// display.textContent = ""
+		}
+	}
 }
 //
 // functions for operations
@@ -109,30 +99,28 @@ function divide(a, b) {
 	return a / b;
 }
 
-
 function operate(temp, op) {
 	temp = parseFloat(temp);
 	if (op == "x") {
-		sol = multiply(temp,parseFloat(display.textContent))
-    console.log(sol)
+		sol = multiply(temp, parseFloat(display.textContent));
+		console.log(sol);
 		// console.log(temp,display.textContent,op)
 	}
 	if (op == "+") {
-    sol = add(temp,parseFloat(display.textContent))
-    console.log(sol)
+		sol = add(temp, parseFloat(display.textContent));
+		console.log(sol);
 		// console.log(temp,display.textContent,op)
 	}
 	if (op == "-") {
-		sol = subtract(temp,parseFloat(display.textContent))
-    console.log(sol)
+		sol = subtract(temp, parseFloat(display.textContent));
+		console.log(sol);
 		// console.log(temp,display.textContent,op)
 	}
 	if (op == "/") {
-		sol = divide(temp,parseFloat(display.textContent))
-    console.log(sol)
+		sol = divide(temp, parseFloat(display.textContent));
+		console.log(sol);
 		// console.log(temp,display.textContent,op)
 	}
-
 }
 
 // function to clear
@@ -141,15 +129,15 @@ function clearCalc() {
 	display.textContent = 0;
 	temp = undefined;
 	op = undefined;
-  state = 0;
-  sol = undefined;
-	removeSelected()
+	state = 0;
+	sol = undefined;
+	removeSelected();
 }
 
-// function to remove selected class 
+// function to remove selected class
 
-function removeSelected(){
-	boxes.forEach(function(box){
-		box.classList.remove("selected")
-	})
+function removeSelected() {
+	boxes.forEach(function (box) {
+		box.classList.remove("selected");
+	});
 }
